48ecb7b9ef672645c17eafc83647d920ecc6b382
rdfong <rfong@iccvlabsrv26.iccluster.epfl.ch>
2017-05-02 23:58:49 +0200

    add yolo2 custom

---
+++ yolo2-pytorch/cfgs/exps/darknet19_exp_boat.py
@@ -0,0 +1,26 @@
+exp_name = 'darknet19_voc12trainval_exp1'
+
+pretrained_fname = 'pretrained_models/darknet19.weights.npz'
+
+start_step = 0
+lr_decay_epochs = {60, 90}
+lr_decay = 1./10
+
+max_epoch = 160
+
+weight_decay = 0.0005
+momentum = 0.9
+init_learning_rate = 1e-3
+
+# for training yolo2
+object_scale = 5.
+noobject_scale = 1.
+class_scale = 1.
+coord_scale = 1.
+iou_thresh = 0.5
+
+# dataset
+imdb_train = 'voc_boat_train1'
+imdb_test = 'voc_boat_val1'
+batch_size = 1
+train_batch_size = 16
