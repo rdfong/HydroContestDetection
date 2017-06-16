b3bea747344abc0c9b691a5509a949d9bb8c28b5
rdfong <rfong@iccvlabsrv25.iccluster.epfl.ch>
2017-06-16 14:52:53 +0200

    results part 2

--- faster_rcnn_pytorch/faster_rcnn/fast_rcnn/config.py
+++ faster_rcnn_pytorch/faster_rcnn/fast_rcnn/config.py
@@ -183,7 +183,7 @@ __C.TEST.MAX_SIZE = 1000
 
 # Overlap threshold used for non-maximum suppression (suppress boxes with
 # IoU >= this threshold)
-__C.TEST.NMS = 0.5
+__C.TEST.NMS = 0.875
 
 # Experimental: treat the (K+1) units in the cls_score layer as linear
 # predictors (trained, eg, with one-vs-rest SVMs).
